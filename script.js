8f2b641fa767be775481d0ef93c99c96cd6ac441
Suraj Kumar <navneetraj07903@gmail.com>
2023-07-08 10:44:59 +0530

    Improved About Section , Main Project and Scrolling for Mobile devices.

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
 // Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
-//console.log(scrollBtn);
 let val;
+
 window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
+  if (document.documentElement.scrollTop > 20) {
     nav.classList.add("sticky");
     scrollBtn.style.display = "block";
-  }else{
+  } else {
     nav.classList.remove("sticky");
     scrollBtn.style.display = "none";
   }
+};
 
-}
-
-// Side NavIgation Menu JS Code
+// Side Navigation Menu JS Code
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = function(){
+
+menuBtn.onclick = function() {
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
   body.style.overflow = "hidden";
   scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function(){
+};
+
+cancelBtn.onclick = function() {
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
   menuBtn.style.pointerEvents = "auto";
   body.style.overflow = "auto";
   scrollBtn.style.pointerEvents = "auto";
-}
+};
 
 // Side Navigation Bar Close While We Click On Navigation Links
 let navLinks = document.querySelectorAll(".menu li a");
 for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
+  navLinks[i].addEventListener("click", function() {
     navBar.classList.remove("active");
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+
+    // Smoothly scroll to the target section
+    let target = document.querySelector(this.hash);
+    let headerOffset = 70; // Adjust this value to match your header's height
+    let elementPosition = target.offsetTop - headerOffset;
+
+    window.scrollTo({
+      top: elementPosition,
+      behavior: "smooth"
+    });
   });
 }
